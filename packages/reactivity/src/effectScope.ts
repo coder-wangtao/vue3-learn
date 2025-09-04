a0d00e360efc3f1188ff2196154fc741026239df
w t <1475929340@qq.com>
2025-09-04 16:06:49 +0800

    .

--- packages/reactivity/src/effectScope.ts
+++ packages/reactivity/src/effectScope.ts
@@ -1,12 +1,21 @@
 export let activeEffectScope;
 
+// const scope = effectScope()
+// scope.run(() => {
+//   const doubled = computed(() => counter.value * 2)
+//   watch(doubled, () => console.log(doubled.value))
+//   watchEffect(() => console.log('Count: ', doubled.value))
+// })
+// scope.stop()
+
 class EffectScope {
   active = true;
-  effects = []; // 收集内部的effect
-  parent;
-  scopes; // 收集作用域的
+  effects = []; // effects 收集内部的 effect
+  parent; // parent:用来记录父 effectScope。
+  scopes; // scopes 收集所有的子 effectScope
   constructor(detached = false) {
-    //detached不会被父亲收集
+    //effectScope 有一个参数叫做 detached，默认是 false,表示当前 effectScope 可以被父亲 effectScope 收集；
+    // true 的话表示当前 effectScope 不可以被父亲 effectScope 收集。
     if (!detached && activeEffectScope) {
       activeEffectScope.scopes || (activeEffectScope.scopes = []).push(this);
     }
